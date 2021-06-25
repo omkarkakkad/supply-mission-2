9cc2e43ab4e24adb546153096f26afea96ff37ee
Omkar Kakkad <72384721+omkarkakkad@users.noreply.github.com>
2021-06-25 11:07:59 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,20 @@
+class Box{
+    constructor(x,y,width,height) {
+        var options = {
+            isStatic: true
+        }
+
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world,this.body);
+    }
+
+    display() {
+        rectMode(CENTER);
+        fill("red");
+        rect(this.body.position.x,this.body.position.y,this.width,this.height);
+        
+    }
+}
